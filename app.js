d42ba92a60591272be5b3d69fe41f7b0a69967e5
Лёха домашний <macarena7777777@gmail.com>
2024-06-02 17:06:48 +0300

    Упражнение - Проверка прав

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-let age = 0;
-console.log(age || 18);
-console.log(age ?? 18);
+let balance = 1001;
+let bonusBalance = 100;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+
+const canBuy = (balance > 1000 || bonusBalance > 100)
+    && !isBanned
+    && !isExist
+    && isSelling;
+
+console.log(`Can I buy the game? ${canBuy ? 'Yes' : 'No'}`);
