f31fb309ac057e15810c4c65976d731488346787
Лёха домашний <macarena7777777@gmail.com>
2024-06-02 11:11:53 +0300

    Арифметическе операторы

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const a = 1;
-console.log(a);
-a = 'test';
-console.log("Привет, мир");
+// Базовые арифметические операторы
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
