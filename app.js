71ea04ea804f1569dfc25e92fe5b626d227b7fc0
Лёха домашний <macarena7777777@gmail.com>
2024-06-04 10:06:40 +0300

    Анонимные функции

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-function logName(name, surname) {
-    console.log(`Моё имя ${name} ${surname}`);
+console.log(powerOfTwo(5));
+function powerOfTwo(num) {
+    return num * num;
 }
 
-const a = logName();
-
-function countDepositSum(depositInUSD, month, rate) {
-    return depositInUSD * (1 + rate / 12) ** month;
+console.log(poft(6));
+const poft = function (num) {
+    return num * num;
 }
-
-const example1 = countDepositSum(1000, 24, 0.12);
-console.log(example1);
-
-console.log(countDepositSum(1000, 48, 0.10));
