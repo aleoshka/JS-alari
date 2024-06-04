bf37cb39a24fa5afafa592110fda13279906c2f0
Лёха домашний <macarena7777777@gmail.com>
2024-06-04 10:33:01 +0300

    Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-function toPower(num, power) {
+function toPower(num, power = 2) {
     const res = num ** power;
     return res;
 }
 
 console.log(toPower(2, 3));
-
-const tpwr = (num, power) => num ** power;
-console.log(tpwr(2, 3));
+console.log(toPower(2));
