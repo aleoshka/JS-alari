5030bdc73e6c4241cce4cd8c0c18542b2d734087
Лёха домашний <macarena7777777@gmail.com>
2024-06-06 20:26:38 +0300

    Упражнение - Функция разбора URL

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const data = [1, 2, 3, 4, 5, 6];
-const [one, two, ...others] = data;
-console.log(one, two, others);
+const url = 'https://purpleschool.ru/course/javascript'
+const url2 = 'https://purpleschoolru/course/javascript'
+
+const getUrlParts = url => {
+    const [protocol, _, host, ...path] = url.split('/');
+    if (protocol === 'https:' || protocol === 'http:') {
+        if (!host.includes('.')) {
+            return;
+        }
+        console.log(`Протокол: ${protocol.split(':')[0]}`);
+        console.log(`Домен: ${host}`);
+        console.log(`Путь: /${path.join('/')}`);
+    }
+}
+
+getUrlParts(url);
