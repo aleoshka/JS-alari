38af8c4a9c1dfd9dc9ecc7ccc18205268017b480
Лёха домашний <macarena7777777@gmail.com>
2024-06-06 21:14:10 +0300

    Цикл for

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const url = 'https://purpleschool.ru/course/javascript'
-const url2 = 'https://purpleschoolru/course/javascript'
+// console.log('Наш баланс 1$');
 
-const getUrlParts = url => {
-    const [protocol, _, host, ...path] = url.split('/');
-    if (protocol === 'https:' || protocol === 'http:') {
-        if (!host.includes('.')) {
-            return;
-        }
-        console.log(`Протокол: ${protocol.split(':')[0]}`);
-        console.log(`Домен: ${host}`);
-        console.log(`Путь: /${path.join('/')}`);
-    }
+for (let i = 1; i < 10; i++) {
+    console.log(`Наш баланс ${i}$`);
 }
-
-getUrlParts(url);
