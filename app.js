e63db22bc05102bce9936e92080c8a1bfab911d6
Лёха домашний <macarena7777777@gmail.com>
2024-06-06 22:08:58 +0300

    Цикл в цикле

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const arr = ['!', 'JS', 'люблю', 'Я'];
-let newArr = [];
+// for (let i = 1; i < 5; i++) {
+//     console.log(`Цикл 1 - ${i}`);
+//     for (let j = 1; j < 5; j++) {
+//         console.log(`Цикл 2 - ${j}`);
+//     }
+// }
 
-for (let i = arr.length - 1; i >= 0; i--) {
-    newArr.push(arr[i]);
+const tasks = [[1, 'Задача 1'], [2, 'Задача 2']];
+
+for (let i = 0; i < tasks.length; i++) {
+    for (let j = 0; j < tasks[i].length; j++) {
+        console.log(tasks[i][j]);
+    }
 }
-
-console.log(newArr.join(' '));
-
-console.log(arr.reverse().join(' '));
