cd5e464b9eb8384ff7d8c21d5f70af36adc98417
Лёха домашний <macarena7777777@gmail.com>
2024-06-16 17:40:00 +0300

    Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-function power(pow) {
-    return function (num) {
-        return num ** pow;
-    }
-}
+// function power(pow) {
+//     return function (num) {
+//         return num ** pow;
+//     }
+// }
+
+const power = pow => num => num ** pow;
 
 const powerOfTwo = power(2);
 console.log(powerOfTwo(5));
@@ -11,4 +13,4 @@ console.log(powerOfTwo(10));
 const powerOfThree = power(3);
 console.log(powerOfThree(5));
 
-console.log(power(5)(4))
+console.log(power(5)(4));
