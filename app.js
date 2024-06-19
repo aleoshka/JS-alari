74d390812e74017c157620c8d964446099063c0f
Лёха домашний <macarena7777777@gmail.com>
2024-06-19 23:26:23 +0300

    Упражнение - Сортировка пользователей

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    skills: [
-        'Программирование',
-        'Готовка'
-    ],
-    eduBasic: 'Школа 10',
-    eduPro: 'МФТИ'
-};
+/* отсортировать пользователей по возрасту */
 
-console.log(user.city);
-console.log(user.skills);
-console.log(user['skills']);
-const level = 'Pro';
-console.log(user['edu' + level]);
-
-// const res = prompt('Введите свойство: ');
-// console.log(user[res]);
-user.city = 'Москва';
-user['city'] = 'Москва';
-console.log(user.city);
-
-user.age = 30;
-user['age'] = 30;
-console.log(user.age);
+const users = [
+    { name: 'Вася', age: 30 },
+    { name: 'Катя', age: 18 },
+    { name: 'Аня', age: 40 },
+    { name: 'Петя', age: 25 },
+];
+console.log(users.sort((a, b) => a.age - b.age));
