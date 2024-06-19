fe193fa4a5ea830dc5b8b705f16940d745387e21
alioshka <macarena7777777@gmail.com>
2024-06-19 11:58:06 +0300

    Преобразование строк

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-/* вытащить имя и фамилию в отдельные перемнные */
-const fullUserName = 'Вася aka Terminator Perdinator Пупкин';
+const str = 'Вася Пупкина';
+console.log(str.includes('а'));
+console.log(str.startsWith('Вас'));
+console.log(str.endsWith('кин'));
+console.log(new String('Вася Пупкин').includes('а'));
 
-const userName = fullUserName.slice(0, fullUserName.indexOf(' '));
-console.log(userName);
-const surname = fullUserName.slice(fullUserName.lastIndexOf(' ') + 1);
-console.log(surname);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('В', 'Ф'));
+console.log(str.replaceAll('а', 'и'));
+console.log(str.replace(/а/g, 'и'));
+
+const str2 = 'Вася Пупкина   \n';
+console.log(str2.trimStart());
