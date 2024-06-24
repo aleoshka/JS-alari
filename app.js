5c27e09637cade37140857a93ebf17fc852029eb
alioshka <macarena7777777@gmail.com>
2024-06-24 11:49:41 +0300

    strict mode

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const user = {
-    name: 'Anton',
-    id: 1,
-    roles: ['Admin']
-};
+'use strict';
 
-// const newUser = Object.assign({}, user);
-// user.name = 'NewUser';
-// console.log(user);
-// console.log(newUser);
+let myCoolVariable = 1;
 
-const newUser2 = {
-    ...user
-};
-newUser2.name = 'NewUser';
-newUser2.roles.push('User');
-console.log(user);
-console.log(newUser2);
+if (true) {
+    myCoolVariable = 3;
+    // function a() {
+    //     console.log('Тест');
+    // }
+}
+// a();
+console.log(myCoolVariable);
+
+// const interface = 7;
+
+function a(a, a) {
+    console.log(a);
+}
+a(1, 1);
