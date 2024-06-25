85a410323955108cfcd9a8c51f53d24c463d088d
alioshka <macarena7777777@gmail.com>
2024-06-25 16:53:26 +0300

    call, apply

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 'use strict'
 
-const b = 1;
-
-const a = {
-    b,
-    getB: function () {
-        return this.b;
-    },
-    getBAlt() {
-        return this.b;
+const audi = {
+    make: 'Audi',
+    model: 'A3',
+    year: 2021,
+    damages: [],
+    addDamage(part, rate) {
+        console.log(
+            `У авто ${this.make} ${this.model} ${this.year} добавлено: - повреждение: ${part} со степенью ${rate}`
+        );
+        this.damages.push({
+            part,
+            rate
+        })
     }
-}
+};
 
-console.log(a.getBAlt());
+// audi.addDamage('Капот', 1)
+
+
+const bmw = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2022,
+    damages: []
+};
+
+// bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2)
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2);
+addDamageFunc.call(bmw, ...['Капот', 1]);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Капот', 1]);
+addDamageFunc.apply(audi, ['Бампер', 2]);
+
