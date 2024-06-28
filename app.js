282375eed1ec50fc9312a9e67901f262c7952a6e
alioshka <macarena7777777@gmail.com>
2024-06-28 12:22:34 +0300

    Работа со стилями и классами

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// document.querySelector('.button').addEventListener('click', function() {
-// 	const input = document.querySelector('.input').value;
-// 	if (!input) {
-// 		return;
-// 	}
-// 	document.querySelector('.panel').innerText = input;
-// 	document.querySelector('.input').value = '';
-// });
-
 function submitForm() {
     const input = document.querySelector('.input').value;
     if (!input) {
@@ -16,13 +7,10 @@ function submitForm() {
     }
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
+    // document.querySelector('.notification').style.display = 'block';
+    // document.querySelector('.notification').classList.add('notification_active');
+    document.querySelector('.notification').classList.remove('notification_hidden');
 }
-
-// document.querySelector('.input').addEventListener('keydown', (e) => {
-//     if (e.code == 'Enter') {
-//         submitForm();
-//     }
-// })
 
 function inputChanged(e) {
     if (e.code == 'Enter') {
