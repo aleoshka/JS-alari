f25f82c581325a7e4eb4b8d41b068820e98e8d90
alioshka <macarena7777777@gmail.com>
2024-06-28 11:55:15 +0300

    Обработка событий с клавиатуры

--- app.js
+++ app.js
@@ -9,11 +9,23 @@
 // 	document.querySelector('.input').value = '';
 // });
 
-function changeClick() {
+function submitForm() {
     const input = document.querySelector('.input').value;
     if (!input) {
         return;
     }
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
+}
+
+// document.querySelector('.input').addEventListener('keydown', (e) => {
+//     if (e.code == 'Enter') {
+//         submitForm();
+//     }
+// })
+
+function inputChanged(e) {
+    if (e.code == 'Enter') {
+        submitForm();
+    }
 }
