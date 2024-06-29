ba31c8b80f0e525980c10da7e0891093e428ad7b
Лёха домашний <macarena7777777@gmail.com>
2024-06-29 15:59:52 +0300

    JSON

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ function inputChanged(e) {
     }
 }
 
-localStorage.setItem('token', 'сау');
-localStorage.setItem('token1', 1);
-localStorage.setItem('token1', true);
-const token1 = localStorage.getItem('token1');
-console.log(typeof token1);
-localStorage.removeItem('token1');
-localStorage.clear();
-
-
+const obj = JSON.parse('{ "a": 1 }');
+console.log(obj);
+const str = JSON.stringify(obj);
+console.log(str);
