18887f64de440029c7920663dbd22991e8a61a71
alioshka <macarena7777777@gmail.com>
2024-07-02 15:17:06 +0300

    Загрузка и сохранение данных

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+/* utils */
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitsArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitsArray)) {
+        habbits = habbitsArray;
+    }
+
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+/* init */
+(() => {
+    loadData();
+})()
